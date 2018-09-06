2361fc18dab89bf92ab9dd39566d72fba98ca1d5
Adam Duff <j.adam.duff@gmail.com>
2018-09-06 02:29:49 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,21 +3,22 @@ class PigLatinizer
   def piglatinize(word)
     consonants = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z', 'w', 'y']
     vowels = ['a', 'e', 'i', 'o', 'u']
-    case word
-    when consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
+    if consonants.include?(word[0]) && consonants.include?(word[1]) && consonants.include?(word[2])
       new_word = word.slice(3, word.length)
       new_word << word.slice(0, 3)
       return new_word << "ay"
-    when consonants.include?(word[0]) && consonants.include?(word[1])
+    elsif consonants.include?(word[0]) && consonants.include?(word[1])
       new_word = word.slice(2, word.length)
       new_word << word.slice(0, 2)
       return new_word << "ay"
-    when consonants.include?(word[0])
+    elsif consonants.include?(word[0])
       new_word = word.slice(1, word.length)
       new_word << word.slice(0, 1)
       return new_word << "ay"
-    when vowels.include?(word[0])
+    elsif vowels.include?(word[0])
       return word << "way"
     end
   end
+
+  
 end
